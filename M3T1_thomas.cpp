65ea0f76d71569a15fc6f66a03770ef42b055056
tariqthomas <thomast0609@student.faytechcc.edu>
2025-02-25 07:37:37 +0000

    mt31_thomas.cpp

---
+++ M3T1_thomas.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+int widthOne, lenghtOne;
+int widthTwo,lenghtTwo;
+
+int areaOne;
+int areaTwo;
+
+cout <<"Enter width and length of the first"<< endl;
+cout <<"rectangle, seperated by a space or newline"<< endl;
+cin>> widthOne>> lenghtOne;
+
+cout <<"Enter width and length of the second"<< endl;
+cout <<"rectangle, seperated by a space or newline"<< endl;
+cin>> widthTwo>> lenghtTwo;
+
+areaOne=widthTwo * lenghtTwo;
+areaTwo=widthTwo * lenghtTwo;
+
+cout<<"rectangle one has an area of "<<areaOne<< endl;
+cout<<"rectangle two has an area of "<<areaTwo<< endl;
+
+
+
+
+
+
+}
